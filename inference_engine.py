a8f26ca5b620a325a70c6178dbc48121e6e7e7a8
jessekerkhof <j.j.kerkhof@student.rug.nl>
2025-12-09 12:25:10 +0100

    added encapsulation and type hints

--- inference_engine.py
+++ inference_engine.py
@@ -1,33 +1,34 @@
-class ForwardEngine:
-    def __init__(self, kb):
-        self.kb = kb
+from knowledge_base import KnowledgeBase
 
-    def run(self):
+class ForwardEngine:
+    def __init__(self, kb: KnowledgeBase):
+        self._kb = kb
+
+    def run(self) -> set:
         """Runs until no new facts can be derived."""
         changed = True
         while changed:
             changed = False
-            for rule in self.kb.rules:
-                if rule.conclusion not in self.kb.facts:
-                    if all(cond in self.kb.facts for cond in rule.conditions):
-                        self.kb.add_fact(rule.conclusion)
+            for rule in self._kb._rules:
+                if rule._conclusion not in self._kb._facts:
+                    if all(cond in self._kb._facts for cond in rule._conditions):
+                        self._kb.add_fact(rule._conclusion)
                         changed = True
-        return self.kb.facts
+        return self._kb._facts
 
 
 class BackwardEngine:
-    def __init__(self, kb):
-        self.kb = kb
+    def __init__(self, kb: KnowledgeBase):
+        self._kb = kb
 
-    def solve(self, goal):
+    def solve(self, goal: str) -> bool:
         """Returns whether goal can be proven."""
-        if goal in self.kb.facts:
+        if goal in self._kb._facts:
             return True
 
-        for rule in self.kb.rules:
-            if rule.conclusion == goal:
-                if all(self.solve(cond) for cond in rule.conditions):
-                    self.kb.add_fact(goal)
+        for rule in self._kb._rules:
+            if rule._conclusion == goal:
+                if all(self.solve(cond) for cond in rule._conditions):
+                    self._kb.add_fact(goal)
                     return True
-
         return False
--- knowledge_base.py
+++ knowledge_base.py
@@ -1,16 +1,18 @@
 class Rule:
-    def __init__(self, conditions, conclusion):
-        self.conditions = conditions
-        self.conclusion = conclusion
+    def __init__(self, conditions: list[str], conclusion: str):
+        self._conditions = conditions
+        self._conclusion = conclusion
 
 
 class KnowledgeBase:
     def __init__(self):
-        self.rules = []  # all rules
-        self.facts = set()  # current known facts
+        self._rules = []  # all rules
+        self._facts = set()  # current known facts
 
-    def add_rule(self, rule):
-        self.rules.append(rule)
+    def add_rule(self, rule) -> None:
+        """Appends a rule to the knowledge base"""        
+        self._rules.append(rule)
 
-    def add_fact(self, fact):
-        self.facts.add(fact)
+    def add_fact(self, fact) -> None:
+        """Adds a fact to the knowledge base"""        
+        self._facts.add(fact)
